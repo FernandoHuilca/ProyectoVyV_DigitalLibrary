32e9598fec3172fe2d879050f4f6661d3f7d4e23
GregorySD1707 <159218483+GregorySD1707@users.noreply.github.com>
2026-06-21 12:00:39 -0500

    quitando url innecesaria y simplificando interfaz de perfil

--- modulo_usuarios/urls.py
+++ modulo_usuarios/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
     # Usamos la vista nativa LoginView y le indicamos la NUEVA ruta de tu plantilla
     path('login/', LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path('perfil_usuario/', PerfilDetailView.as_view(template_name='usuarios/perfil_usuario.html'), name='perfil_usuario'),
-    path('perfil_usuario/<int:pk>/', PerfilDetailView.as_view(template_name='usuarios/perfil_usuario_edicion.html'),
-         name='perfil_usuario_edicion'),
     path('perfil_usuario/<int:pk>/editar/', PerfilUpdateView.as_view(template_name='usuarios/perfil_usuario_edicion.html'),
          name='editar_perfil'),
 ]
